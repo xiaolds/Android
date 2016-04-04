55e5c762a5a791a80e1bd5c5f54864a384fa3a51
xiaolds <xiao.lds@163.com>
2016-04-04 11:04:35 +0800

    Draw the rec

--- CameraRuler/app/src/main/java/com/datasure/cameraruler/RulerActivity.java
+++ CameraRuler/app/src/main/java/com/datasure/cameraruler/RulerActivity.java
@@ -13,17 +13,7 @@ public class RulerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_ruler);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
--- CameraRuler/app/src/main/java/com/datasure/cameraruler/RulerView.java
+++ CameraRuler/app/src/main/java/com/datasure/cameraruler/RulerView.java
@@ -1,11 +1,16 @@
 package com.datasure.cameraruler;
 
+/**
+ * 最内部的View
+ */
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,11 +21,14 @@ import android.view.View;
  */
 public class RulerView extends View {
 
-    private int measuredHeight;     //margin height
-    private int measuredWidth;      //margin width
 
-    private int width;              //padding width
-    private int height;             //padding height
+    private int width = 200;              //padding width
+    private int height = 200;             //padding height
+
+    private int recL;
+    private int recT;
+    private int recR;
+    private int recB;
 
     private String text;            //the text will draw on the canvas
 
@@ -34,17 +42,6 @@ public class RulerView extends View {
 
     public RulerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-//        measuredHeight = attrs.getAttributeResourceValue(null,"marginH",0);
-//        measuredWidth = attrs.getAttributeResourceValue(null, "marginW", 0);
-//
-//        width = attrs.getAttributeResourceValue(null, "width", 0);
-//        height = attrs.getAttributeResourceValue(null, "height", 0);
-//        int textId = attrs.getAttributeResourceValue(null, "text", 0);
-//
-//
-//        text = context.getResources().getText(textId).toString();
-
     }
 
     public RulerView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -59,33 +56,71 @@ public class RulerView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        //调用measure()
-
+//        setMeasuredDimension(200,200);
     }
 
     /**
-     * 响应触摸事件
+     * 响应触摸事件,共两种，一种是移动，一种是改变大小
      * @param event
      * @return
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        float touchX = event.getX();  //手指触摸的横坐标
+        float touchY = event.getY();  //手指触摸的纵坐标
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            //先确定触摸操作在范围内
+
+            if(touchX < recL || touchX > recR ||
+                    touchY < recT || touchY > recB){
+                return true;
+            }
+        }
+
+        float rawX = event.getRawX();
+        float rawY = event.getRawY();
+
+        float L = rawX - recL;
+        float R = recR - rawY;
+        float T = rawY - recT;
+        float B = recB - rawY;
+        if(event.getAction() == MotionEvent.ACTION_MOVE){
+            //处理移动效果
+            //change the rec
+            recL = (int)(touchX - L);
+            recR = (int)(touchX + R);
+            recT = (int)(touchY - T);
+            recB = (int)(touchY + B);
+            invalidate();
+            Log.e("L", String.valueOf(recL));
+            Log.e("T", String.valueOf(recT));
+            Log.e("R", String.valueOf(recR));
+            Log.e("B", String.valueOf(recB));
+        }
+
+        return true;
+
     }
 
+
     /**
-     * 绘制画面
+     * 绘制一个绿色的框
      * @param canvas
      */
     @Override
     protected void onDraw(Canvas canvas) {
 //        super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        Rect rect = new Rect(100,100,100,100);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-        canvas.drawRect(rect,paint);
-        canvas.drawText("Test For drawing text!",100,100,paint);
+        //change the size
+        recL = getMeasuredWidth()/2 - width/2;
+        recT = getMeasuredHeight()/2 - height/2;
+        recR = recL + width;
+        recB = recT + height;
+        Rect rect = new Rect(recL, recT, recR, recB);
+        //paint the rec
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(rect, paint);
+
     }
 }
